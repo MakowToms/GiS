79892b6faff0fa38f34a067c0c3f7008f6be6a75
pi1415926535 <makowskit2@student.mini.pw.edu.pl>
2020-05-30 01:55:38 +0200

    plot 3 plots in one

--- plots/plots.py
+++ plots/plots.py
@@ -1,20 +1,19 @@
 import matplotlib.pyplot as plt
 
-"""
-Code copied from my another repository
-Not all method are useful in this project
-"""
 
-
-def plot_measure_results_data(errors, error=None, title_base='MSE', title_ending=' through epochs', labels=["base model", "momentum model", "RMSProp model"], colors=["green", "blue", "red", "magenta", "black", "brown", "orange", "yellow",  "red", "magenta"], xlabel='epochs', ylabel='MSE', from_error=0, show=True, x=None, save_name=None, log=False, log_y=False):
+def plot_measure_results_data(errors, error=None, error_type=None, title_base='MSE', title_ending=' through epochs', labels=["base model", "momentum model", "RMSProp model"], colors=["green", "blue", "red", "magenta", "black", "brown", "orange", "yellow",  "red", "magenta"], xlabel='epochs', ylabel='MSE', from_error=0, show=True, x=None, save_name=None, log=False, log_y=False):
     n = len(errors[0])
     if x is None:
         x = [i for i in range(from_error, n)]
     for i in range(len(errors)):
         plt.plot(x, errors[i][from_error:], color=colors[i])
     if error is not None:
-        for i in range(len(errors)):
-            plt.plot(x, [error[i]] * len(errors[i][from_error:]), color=colors[i], linestyle=':')
+        if error_type is not None:
+            for i in range(len(errors)):
+                plt.plot(x, error[i][from_error:], color=colors[i], linestyle=':')
+        else:
+            for i in range(len(errors)):
+                plt.plot(x, [error[i]] * len(errors[i][from_error:]), color=colors[i], linestyle=':')
     if log:
         plt.xscale('log')
     if log_y:
@@ -38,3 +37,9 @@ def create_8_plots(times, nmis, modularities, n_communities, modularities_base, 
     plot_measure_results_data(nmis[4:], x=x, labels=labels[4:], title_base="Mutual information", title_ending="", ylabel="Normalized mutual information", xlabel=xlabel, save_name="plots/" + base_save_name + "2_nmi")
     plot_measure_results_data(modularities[4:], error=modularities_base, x=x, labels=labels[4:], title_base="Modularities", title_ending="", ylabel="Link belong modularity", xlabel=xlabel, save_name="plots/" + base_save_name + "2_modularity")
     plot_measure_results_data(n_communities[4:], error=n_communities_base, x=x, labels=labels[4:], title_base="Communities", title_ending="", ylabel="Number of communities", xlabel=xlabel, save_name="plots/" + base_save_name + "2_ncoms")
+
+
+def create_3_plots(nmis, modularities, n_communities, modularities_base, n_communities_base, labels, x, xlabel, base_save_name):
+    plot_measure_results_data(nmis, x=x, labels=labels, title_base="Mutual information", title_ending="", ylabel="Normalized mutual information", xlabel=xlabel, save_name="plots/" + base_save_name + "1_nmi")
+    plot_measure_results_data(modularities, error=modularities_base, error_type="", x=x, labels=labels, title_base="Modularities", title_ending="", ylabel="Link belong modularity", xlabel=xlabel, save_name="plots/" + base_save_name + "1_modularity")
+    plot_measure_results_data(n_communities, error=n_communities_base, error_type="", x=x, labels=labels, title_base="Communities", title_ending="", ylabel="Number of communities", xlabel=xlabel, save_name="plots/" + base_save_name + "1_ncoms")
